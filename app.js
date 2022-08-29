cfe1a4da63cf3ffba573510e8cb9e47454aff023
MASAHIRO AMEMIYA <ame6masa6@gmail.com>
2022-08-29 18:04:16 +0900

    変数を学ぶ

---
+++ app.js
@@ -0,0 +1,27 @@
+// 文字列型
+let hello = "Hello World";
+alert(hello);
+
+// 数値型
+let number1 = 123;
+let number2 = 1.5;
+
+// 真偽型
+let isChecked = true;
+if (isClecked) {
+  console.log("trueの場合の処理");
+}
+
+//null
+let foo = null;
+
+//リテラル
+const array = [1,2,3,4];
+//newで作成する
+const array2 = new Array(1,2,3,4);
+
+//サイズが3の配列ができる
+const array1 = new Array(3);
+//1,2が入った配列ができる
+const array2 = new Array(1,2);
+
